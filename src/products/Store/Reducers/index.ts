6467fd80bd0169dec177e7c427701b566f6bfffb
brunoramos <brunotolomeotti@gmail.com>
2018-12-13 21:02:51 -0200

    selectors

---
+++ src/products/Store/Reducers/index.ts
@@ -0,0 +1,20 @@
+import {ActionReducerMap, createFeatureSelector, createSelector} from "@ngrx/store";
+
+import {PizzaState, reducer, getPizzas, getPizzasLoaded, getPizzasLoading} from "./pizzas.reducer";
+
+export interface ProductsState {
+  pizzas: PizzaState
+}
+
+export const reducers: ActionReducerMap<ProductsState> = {
+  pizzas: reducer,
+  };
+
+export const getProductsState = createFeatureSelector<ProductsState>('products');
+
+//pizza state
+export const getPizzaState = createSelector(getProductsState, (state: ProductsState) => state.pizzas);
+
+export const getAllPizzas = createSelector(getPizzaState, getPizzas);
+export const getLoadedPizzas = createSelector(getPizzaState, getPizzasLoaded);
+export const getLoadingPizzas = createSelector(getPizzaState, getPizzasLoading);
